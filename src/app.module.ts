f2dcb692bba863ee89a02bd48601c8faad3d984f
Emmanuel Manzanilla <pcoriente.dev.jrs@gmail.com>
2025-07-05 14:09:07 -0600

    creación de los modules de usuario, unidades y clientes

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-
+import { UsuariosModule } from './app/usuarios/usuarios.module';
+import { UnidadesModule } from './app/unidades/unidades.module';
 @Module({
-  imports: [],
+  imports: [UsuariosModule, UnidadesModule],
   controllers: [AppController],
   providers: [AppService],
 })
